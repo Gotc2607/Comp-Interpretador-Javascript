52fe0664241cc64fa8e139b1ccff2fb1ff682b6b
ArturFGaldino <fernandesartur4@gmail.com>
2026-06-10 15:55:36 -0300

    test: adiciona novo teste para console.log e suas expressões

---
+++ testes/teste_14_console.js
@@ -0,0 +1,9 @@
+let saudacao = "Testando o console.log!";
+console.log(saudacao);
+
+console.log("O resultado da expressao e:");
+console.log(100 + 50 * 2);
+
+let a = 10;
+let b = 20;
+console.log(a + b);
