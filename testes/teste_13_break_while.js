e02ed298ac72394dcced10fd45e3cdd2f0a3fe67
Yasmim de Souza Santos <241040860@aluno.unb.br>
2026-06-13 13:38:42 -0300

    test: adicionando testes para o break e continue

---
+++ testes/teste_13_break_while.js
@@ -0,0 +1,14 @@
+i = 0;
+while (i < 10) {
+    if (i == 3) break;
+    i = i + 1;
+}
+i;
+
+j = 0;
+while (j < 5) {
+    j = j + 1;
+    if (j == 2) break;
+    j;
+}
+j;
---
+++ testes/teste_15_break_continue_for.js
@@ -0,0 +1,10 @@
+for (i = 0; i < 10; i = i + 1) {
+    if (i == 4) break;
+    i;
+}
+i;
+
+for (j = 0; j < 6; j = j + 1) {
+    if (j == 2) continue;
+    j;
+}
